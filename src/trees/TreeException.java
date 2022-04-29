db3ed80d0e4ee6e58857f967b9f8e64199f6accd
Jugraj <pjugraj@yahoo.com>
2022-04-29 12:09:08 -0700

    tree exception update

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,12 @@
 package trees;
 
-public class TreeException extends ???
+import java.io.IOException;
+
+public class TreeException extends IOException
 {
 	public TreeException(String s)
 	{
+		super(s); 
 		// Pass s into a superclass ctor.
 	}
 }
